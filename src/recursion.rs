51b98826b4b80dad9a44f6e53e82af0d2bbad653
Veikko Rytivaara <veikko.rytivaara@bittium.com>
2017-05-20 15:46:50 +0300

    Add delay to recursion.

--- src/recursion.rs
+++ src/recursion.rs
@@ -1,9 +1,12 @@
 use tuple;
+use std::{thread, time};
 
 /// PRIVATE: Example using function call recursion.
 fn using_recursion_and_tuples(number: u8) {
     println!(">>>> [{}] using_recursion_and_tuples", file!());
+    let sleep_duration = time::Duration::from_millis(100);
     println!("{}", tuple::use_match_with_tuples(number));
+    thread::sleep(sleep_duration);
     if number < 100 {
         using_recursion_and_tuples(number + 1);
     }
