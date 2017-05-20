a009d9394db04713a9b4b2e6b195cb350c848c29
Veikko Rytivaara <veikko.rytivaara@bittium.com>
2017-05-20 16:10:18 +0300

    Add more comments to recusrsion.rs.

--- src/recursion.rs
+++ src/recursion.rs
@@ -2,6 +2,9 @@ use tuple;
 use std::{thread, time};
 
 /// PRIVATE: Example using function call recursion.
+///
+/// Pretty much the most basic recursion example where
+/// we init the recusrsion via function call with a count argument.
 fn using_recursion_and_tuples(number: u8) {
     println!(">>>> [{}] using_recursion_and_tuples", file!());
     let sleep_duration = time::Duration::from_millis(100);
